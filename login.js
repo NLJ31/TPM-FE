1799391ed6c0b8316cce8b6d8f436706bdc60ac6
NLJ31 <sensen.nelson@gmail.com>
2023-01-16 12:17:45 +0700

    Update login information

---
+++ login.js
@@ -0,0 +1,18 @@
+
+function showPassword(){
+    var x = document.getElementById("Passowrd-login");
+    var y = document.getElementById("hide1");
+    var z = document.getElementById("hide2");
+    
+    if(x.type==='password'){
+        x.type="text";
+        y.style.display="none";
+        z.style.display="block";
+    }  
+    else{
+        x.type="password";
+        y.style.display="block";
+        z.style.display="none";
+        
+    }
+}
